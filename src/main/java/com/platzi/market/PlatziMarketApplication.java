6f703ab8e0560f7b989fb21b237fe677d1d31e59
CHEO <cheo@MacBook-Pro-de-Cheo.local>
2023-03-01 19:25:35 -0500

    codigo reestablecido

--- src/main/java/com/platzi/market/PlatziMarketApplication.java
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -3,10 +3,7 @@ package com.platzi.market;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-<<<<<<< HEAD
-//inicio de la aplicación prueba
-=======
->>>>>>> 6ac5957a85beb807fe0f94f0c56b513e0ef9dfe4
+//inicio de la aplicación
 @SpringBootApplication
 public class PlatziMarketApplication {
 
