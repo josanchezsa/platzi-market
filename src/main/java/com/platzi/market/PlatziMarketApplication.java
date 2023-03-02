c93e7ae679ceadef322ee3fb7d4e448e3455548c
CHEO <cheo@MacBook-Pro-de-Cheo.local>
2023-03-01 19:22:49 -0500

    prueba

--- src/main/java/com/platzi/market/PlatziMarketApplication.java
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -3,7 +3,7 @@ package com.platzi.market;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//inicio de la aplicación
+//inicio de la aplicación prueba
 @SpringBootApplication
 public class PlatziMarketApplication {
 
